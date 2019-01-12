f2ee0685414aca7b7fcf0247d632d04e5091149c
abdul <abdul@codilar.com>
2019-01-12 17:27:07 +0530

    carousel added

--- app/code/Codilar/Afbt/view/frontend/web/js/afbt_block_handler.js
+++ app/code/Codilar/Afbt/view/frontend/web/js/afbt_block_handler.js
@@ -1,4 +1,4 @@
-define(['jquery', 'underscore'], function ($, _) {
+define(['jquery', 'underscore', 'owlCarousel'], function ($, _, owlCarousel) {
     return function (config) {
         var Afbt = {
             fetchUrl: config.afbtFetchUrl,
@@ -22,6 +22,43 @@ define(['jquery', 'underscore'], function ($, _) {
                                 $(".afbt-container").html(template({
                                     productsData: response
                                 }));
+                                var owl = $("#fbt-carousel-blog");
+                                owl.owlCarousel({
+                                    loop:false,
+                                    nav:true,
+                                    margin:15,
+                                    navText: ['<div class="left-arrow">', '<div class="right-arrow">'],
+                                    responsive:{
+                                        0:{
+                                            items:1,
+                                            margin:0
+                                        },
+                                        600:{
+                                            items:1,
+                                            margin:0
+                                        },
+                                        1000:{
+                                            items:3
+                                        }
+                                    }
+                                });
+                                checkClasses();
+                                carousel.on('translated.owl.carousel', function(event) {
+                                    checkClasses();
+                                });
+
+                                function checkClasses(){
+                                    var total = $('.fbt-container .owl-stage .owl-item.active').length;
+
+                                    $('.fbt-container .owl-stage .owl-item').removeClass('lastActiveItem');
+
+                                    $('.fbt-container .owl-stage .owl-item.active').each(function(index){
+                                        if (index === total - 1 && total>1) {
+                                            // this is the last one
+                                            $(this).addClass('lastActiveItem');
+                                        }
+                                    });
+                                }
                             });
                         } else {
                             $(".afbt-container").html("");
