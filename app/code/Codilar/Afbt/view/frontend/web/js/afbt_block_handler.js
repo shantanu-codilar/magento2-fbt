cdf530ec4b8d059ebe186baf2810104ed9456393
shaandg <shantanu@codilar.com>
2019-01-13 17:58:53 +0530

    Afbt restricted for configurable products

--- app/code/Codilar/Afbt/view/frontend/web/js/afbt_block_handler.js
+++ app/code/Codilar/Afbt/view/frontend/web/js/afbt_block_handler.js
@@ -89,6 +89,36 @@ define(['jquery', 'underscore', 'owlCarousel'], function ($, _, owlCarousel) {
                         });
                     }
                 });
+            },
+            getSimpleProductId: function () {
+                var selected_options = {};
+                jQuery('div.swatch-attribute').each(function(k,v){
+                    var attribute_id    = jQuery(v).attr('attribute-id');
+                    var option_selected = jQuery(v).attr('option-selected');
+                    //console.log(attribute_id, option_selected);
+                    if(!attribute_id || !option_selected){ return;}
+                    selected_options[attribute_id] = option_selected;
+                });
+
+                var product_id_index = jQuery('[data-role=swatch-options]').data('mageSwatchRenderer').options.jsonConfig.index;
+                var found_ids = [];
+                //console.log(product_id_index);
+                jQuery.each(product_id_index, function(product_id,attributes){
+                    //console.log(product_id);
+                    var productIsSelected = function(attributes, selected_options){
+                        return _.isEqual(attributes, selected_options);
+                    }
+                    if(productIsSelected(attributes, selected_options)){
+                        found_ids.push(product_id);
+                    }
+                });
+
+                //console.log(found_ids);
+
+                if (found_ids.length) {
+                    var selected_product_id = found_ids[0];
+                    console.log(selected_product_id);
+                }
             }
         };
         /** initialize the afbt widget */
